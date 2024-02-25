3931cb0e62579723c00357d48aba52a3bca8aeb4
Kevin <kevin-k-is-back@hotmail.com>
2024-02-25 14:28:02 +0100

    .

---
+++ algemene_functies.py
@@ -0,0 +1,12 @@
+def mijn_functie_1(argument):
+    if argument == 2:
+        return 4
+    elif argument == 4:
+        return 16
+    elif argument == 10:
+        return 100
+    elif argument == 12:
+        return 144
+    else:
+        return None
+
