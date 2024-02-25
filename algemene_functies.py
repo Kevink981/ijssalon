fb805ece3b5cad9fb7ab7e59548ddcebeb9274e1
Kevin <kevin-k-is-back@hotmail.com>
2024-02-25 22:49:26 +0100

    .

--- algemene_functies.py
+++ algemene_functies.py
@@ -1,25 +1,10 @@
 def mijn_functie_1(argument):
-    if argument == 2:
-        return 4
-    elif argument == 4:
-        return 16
-    elif argument == 10:
-        return 100
-    elif argument == 12:
-        return 144
-    else:
-        return None
+    return argument ** 2
 
-
-
-def mijn_functie_2(argument):
-    if argument == (12, 3):
-        return [15, 9, 36, 4]
-    elif argument == (12, 2):
-        return [14, 10, 24, 6]
-    elif argument == (10, 5):
-        return [15, 5, 50, 2]
-    elif argument == (100, 20):
-        return [120, 80, 2000, 5]
-    else:
-        return None
+def mijn_functie_2(a, b):
+    uitvoer_lijst = []
+    uitvoer_lijst.append(a + b)
+    uitvoer_lijst.append(a - b)
+    uitvoer_lijst.append(a * b)
+    uitvoer_lijst.append(a / b)
+    return uitvoer_lijst
