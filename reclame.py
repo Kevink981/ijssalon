18e5f1b46509a1cb54410896b13e531e029aaec4
Kevin <kevin-k-is-back@hotmail.com>
2024-02-25 14:32:53 +0100

    .

--- reclame.py
+++ reclame.py
@@ -4,6 +4,9 @@ def aanbieding_1(smaak, prijs, korting):
     return f"Vandaag in de aanbieding: emmertje ijs (1 liter) in de smaak {smaak}, van {prijs:.2f} euro voor {nieuwe_prijs:.2f} euro."
 
 
-def inkomsten_totaal(inkomsten):
-    return sum(inkomsten)
+def inkomsten_totaal(inkomsten, btw):
+
+    totaal_inkomsten = sum(inkomsten)
+    btw_bedrag = totaal_inkomsten * btw
+    return f"Het totaal van alle inkomsten van deze week is {totaal_inkomsten:.2f} euro, waarover {btw_bedrag:.2f} euro btw betaald dient te worden."
 
