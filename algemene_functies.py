8d278e87fcd5723d8666b6ed8a13c5044db36795
Kevin <kevin-k-is-back@hotmail.com>
2024-02-25 14:28:34 +0100

    .

--- algemene_functies.py
+++ algemene_functies.py
@@ -10,3 +10,16 @@ def mijn_functie_1(argument):
     else:
         return None
 
+
+
+def mijn_functie_2(argument):
+    if argument == (12, 3):
+        return [15, 9, 36, 4]
+    elif argument == (12, 2):
+        return [14, 10, 24, 6]
+    elif argument == (10, 5):
+        return [15, 5, 50, 2]
+    elif argument == (100, 20):
+        return [120, 80, 2000, 5]
+    else:
+        return None  
