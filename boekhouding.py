670b7593dfc1502a06aea8ee60cb204d7a98f412
Kevin <kevinkaal@hotmail.com>
2024-02-26 15:16:15 +0100

    .

--- boekhouding.py
+++ boekhouding.py
@@ -1,3 +1,4 @@
+import csv
 from helper import *
 from presentatie import *
 
@@ -12,3 +13,9 @@ totaal_inkomsten = som(inkomsten)
 
 #print("Totale inkomsten:", totaal_inkomsten)
 presenteer(inkomsten, totaal_inkomsten)
+
+
+with open('boekhouding.csv', 'w', newline='') as csvfile:
+    writer = csv.writer(csvfile, delimiter=';')
+    for key, value in inkomsten.items():
+        writer.writerow([key, value])
